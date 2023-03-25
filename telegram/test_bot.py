cce7ea3417642cf595528aa984306acbae72351d
Oleg Sokolov <klaus888@yandex.ru>
2023-03-25 14:23:47 +0300

    1

--- telegram/test_bot.py
+++ telegram/test_bot.py
@@ -1,34 +1,24 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
 
+from aiogram import Bot, Dispatcher, executor, types
 
 logging.basicConfig(level=logging.INFO)
 
-token = "6241591643
-:
-AAG3Fj6XQMejHi5
-print()
--
-mLsc1rjKcKkyXDsYiYk
+token = "6241591643:AAG3Fj6XQMejHi5-mLsc1rjKcKkyXDsYiYk"
 
 bot = Bot(token=token)
 
-dp = Dispatcher()
+dp = Dispatcher(bot=bot)
 
-@dp.message(Command('start'))
+@dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
-    await message.reply('test all right')
-
-async def main():
-    await dp.start_polling(bot)
+    await message.reply('Get start!@')
 
 
 
 # if __name__ == '__main__':
 
-asyncio.run(main())
-
+executor.start_polling(dp, skip_updates=True)
 
 
