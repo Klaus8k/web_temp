df42ef7c47011e975349bc4e5a885f58deb7208b
Oleg Sokolov <klaus888@yandex.ru>
2023-03-27 15:54:08 +0300

    teleg

--- telegram/test_bot.py
+++ telegram/test_bot.py
@@ -1,26 +1,60 @@
 import asyncio
 import logging
+from random import randint
 
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.utils import exceptions
 
 logging.basicConfig(level=logging.INFO)
 
-token = "6241591643
-:AAG3Fj6XQMejHi5
--mLsc1rjKcKkyXDsYiYk"
+what_it_is = '6241591643'
 
-bot = Bot(token=token)
+token = what_it_is + ":AAG3Fj6XQMejHi5-mLsc1rjKcKkyXDsYiYk"
+print(token)
+
+bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
 
 dp = Dispatcher(bot=bot)
 
-@dp.message_handler(commands='start')
+# Хэндлер исключений
+bots_exeptions = [exceptions.BotBlocked,]
+
+
+@dp.errors_handler(exception=bots_exeptions)
+async def errors_bot(update: types.Update, exeption: object):
+    pass
+
+
+# Хэндлер ответа
+@dp.message_handler(commands=['start', 'help', '1-10'])
 async def cmd_start(message: types.Message):
-    await message.reply('Get start!@')
+    print(message)
+    if message.get_command() == r'/start':
+        await message.reply('Get start!@')
+    elif message.get_command() == r'/help':
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        buttons = ['Menu', 'random', 'Order']
+        keyboard.add(*buttons)
+        await message.answer('Menu:', reply_markup=keyboard)
 
 
+@dp.message_handler(lambda message: message.text == 'random')
+async def random(message: types.Message):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(
+        'rand 1-100', callback_data='start_rand'))
+    await message.answer('push the button', reply_markup=keyboard)
 
-# if __name__ == '__main__':
 
-executor.start_polling(dp, skip_updates=True)
+@dp.callback_query_handler(text='start_rand')
+async def send_rand(call: types.CallbackQuery):
+    await call.message.answer(str(randint(1, 100)))
 
 
+@dp.message_handler(lambda message: message.text in ['order', 'Order'])
+async def order(message: types.Message):
+    await message.answer('Link to <a href="https://sokolol.ru/order_cost/">order</a>')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
