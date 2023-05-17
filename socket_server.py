5ac85aeacfdb5ee4757f3009d5b3e59f2f70bcb2
Oleg Sokolov <klaus888@yandex.ru>
2023-05-17 14:34:00 +0300

    add serv client simple

---
+++ socket_server.py
@@ -0,0 +1,25 @@
+# import requests
+import socket
+
+HOST = 'localhost'
+PORT = 5000
+
+
+
+# выход по ctrl C
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind((HOST, PORT))
+        sock.listen()
+        conn, addr = sock.accept()
+
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+                print(f'connect by {addr} -- {data.decode(encoding="utf-8")}')
+
+    break
+
