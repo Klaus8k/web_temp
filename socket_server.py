b7c711976a6047143092948a777a3d01356cc1da
Oleg Sokolov <klaus888@yandex.ru>
2023-05-23 14:29:57 +0300

    eee

--- socket_server.py
+++ socket_server.py
@@ -10,18 +10,18 @@ with open('./files_for_docker/1.txt', 'r') as file:
 
 count = 0
 
-while count < 3:
+while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.bind((HOST, PORT))
         sock.listen()
         conn, addr = sock.accept()
 
         with conn:
-            while True:
+            while count < 3:
                 data = conn.recv(1024)
                 if not data:
                     break
                 conn.sendall(data)
                 print(f'connect by {addr} -- {data.decode(encoding="utf-8")} --{x}-- {datetime.datetime.now()}')
-    count += 1
+            count += 1
     
