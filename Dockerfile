76f42381a011cb72704488742c2e15e2832de2fa
Klaus8k <tipkor@mail.ru>
2023-05-23 14:19:35 +0300

    11

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM python:3.8
 
-WORKDIR /usr/src/app
+WORKDIR /app
 
-COPY . .
-
-RUN pip install --no-cache-dir -r requirements.txt
+COPY socket_server.py socket_server.py
 
 EXPOSE 5000
 
-CMD ["python", "./manage.py", "runserver", "0.0.0.0:5000"]
+ENV TZ Europe/Moscow
+
+CMD ["python", "./socket_server.py"]
--- socket_server.py
+++ socket_server.py
@@ -1,25 +1,27 @@
 # import requests
 import socket
+import datetime
 
-HOST = '79.133.181.123'
-PORT = 8000 
+HOST = '0.0.0.0'
+PORT = 5000
 
+with open('./files_for_docker/1.txt', 'r') as file:
+	x = file.read()
 
-
-# выход по ctrl C
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.bind((HOST, PORT))
-        sock.listen()
+count = 0
+while count < 3:
+	count += 1
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+		sock.bind((HOST, PORT))
+		sock.listen()
         conn, addr = sock.accept()
-
         with conn:
             while True:
                 data = conn.recv(1024)
                 if not data:
                     break
                 conn.sendall(data)
-                print(f'connect by {addr} -- {data.decode(encoding="utf-8")}')
+                print(f'connect by {addr} -- {data.decode(encoding="utf-8")} --{x}-- {datetime.datetime.now()}')
 
     break
 
