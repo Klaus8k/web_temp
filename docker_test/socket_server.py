b547c64a3d0a21f6d475c764d141b9871eeb80ef
Oleg Sokolov <klaus888@yandex.ru>
2023-05-24 16:26:12 +0300

    1

--- docker_test/socket_server.py
+++ docker_test/socket_server.py
@@ -17,13 +17,16 @@ while True:
 
         with conn:
             while count < 3:
+
                 with open('1/1.txt', 'r') as file:
 	                adder = file.read()
+
                 data = conn.recv(1024)
+
                 if not data:
                     break
                 data_str = data.decode(encoding="utf-8")
                 responce = f'Получено -- {data_str} Добавка из файла: {adder} Время:{datetime.datetime.now()}'
-				conn.sendall(bytes(responce, encoding='utf-8'))
+                conn.sendall(bytes(responce, encoding='utf-8'))
                 count += 1
     
