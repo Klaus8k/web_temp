1171df58ed659ee94befc02ad6bc72e80dbffefe
Oleg Sokolov <klaus888@yandex.ru>
2023-05-24 16:55:38 +0300

    111111

--- docker_test/socket_server.py
+++ docker_test/socket_server.py
@@ -6,9 +6,6 @@ HOST = '0.0.0.0'
 PORT = 5000
 
 
-
-count = 0
-
 while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.bind((HOST, PORT))
@@ -16,17 +13,15 @@ while True:
         conn, addr = sock.accept()
 
         with conn:
-            while count < 3:
                 data = conn.recv(1024)
 
                 if not data:
                     break
-                
+
                 data_str = data.decode(encoding="utf-8")
                 responce = f'Получено -- {data_str} Добавка из файла: {adder} Время:{datetime.datetime.now()}'
                 conn.sendall(bytes(responce, encoding='utf-8'))
                 print(responce)
-                count += 1
 
                 with open('1/1.txt', 'r') as file:
                     adder = file.read()
