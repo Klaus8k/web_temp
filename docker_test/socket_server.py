94f6a3092e9ae56dc694fbd007910deb7635cc45
Oleg Sokolov <klaus888@yandex.ru>
2023-05-24 16:58:02 +0300

    1

--- docker_test/socket_server.py
+++ docker_test/socket_server.py
@@ -5,25 +5,26 @@ import socket
 HOST = '0.0.0.0'
 PORT = 5000
 
+def read_file():
+    with open('1/1.txt', 'r') as file:
+        return file.read()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind((HOST, PORT))
+    sock.listen()
+    conn, addr = sock.accept()
 
 while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.bind((HOST, PORT))
-        sock.listen()
-        conn, addr = sock.accept()
+    with conn:
+            data = conn.recv(1024)
 
-        with conn:
-                data = conn.recv(1024)
-                adder = '1st request'
+            if not data:
+                break
 
-                if not data:
-                    break
+            data_str = data.decode(encoding="utf-8")
+            responce = f'Получено -- {data_str} Добавка из файла: {read_file()} Время:{datetime.datetime.now()}'
+            conn.sendall(bytes(responce, encoding='utf-8'))
+            print(responce)
 
-                data_str = data.decode(encoding="utf-8")
-                responce = f'Получено -- {data_str} Добавка из файла: {adder} Время:{datetime.datetime.now()}'
-                conn.sendall(bytes(responce, encoding='utf-8'))
-                print(responce)
 
-                with open('1/1.txt', 'r') as file:
-                    adder = file.read()
     
