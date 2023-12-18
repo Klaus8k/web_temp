4ed39aa31fb5d560375a78117eaf951d86ebf978
Oleg Sokolov <klaus888@yandex.ru>
2023-12-18 18:28:16 +0300

    annagram

--- lay_1/data_types.py
+++ lay_1/data_types.py
@@ -1,9 +1,11 @@
-s = "angagram"
-t = "nagarasm"
+s = "a"
+t = "a"
 
-
+# попробовать сделать сет из второго и сравнить с количеством ключей первого
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
         dict_letter = {i: s.count(i) for i in s}
         for i in dict_letter.keys():
             if dict_letter[i] == t.count(i):
