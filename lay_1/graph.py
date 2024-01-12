d76118c038d9862455ae18cdcfa0de107f24d6c5
Oleg Sokolov <klaus888@yandex.ru>
2024-01-12 18:27:02 +0300

    *

--- lay_1/graph.py
+++ lay_1/graph.py
@@ -1,14 +1,11 @@
 from collections import deque
 
-graph = {}
-graph['you'] = ['alice', 'bob', 'claire']
-graph['bob'] = ['anju', 'peggy']
-graph['alice'] = ['peggy']
-graph['claire'] = ['thom', 'jonny']
-graph['anju'] = []
-graph['peggy'] = []
-graph['thom'] = []
-graph['jonny'] = []
+graph = {'you': ['alice', 'bob', 'claire'],
+         'bob': ['anju', 'peggy'],
+         'alice': ['peggy'],
+         'claire': ['thom', 'jonny'],
+         'anju': [], 'peggy': [],
+         'thom': [], 'jonny': []}
 
 def mango_seller_search(name):
     search_queue = deque()
@@ -17,7 +14,7 @@ def mango_seller_search(name):
     while search_queue:
         person = search_queue.popleft()
         if not person in searched:
-            if person[-1] == 'y':
+            if person[-1] == 'u':
                 print(person + ' - mango seller')
                 return True
             else:
@@ -25,20 +22,18 @@ def mango_seller_search(name):
                 searched.append(person)
     return False
 
+
 mango_seller_search('you')
 
 
 # Взвешенный граф
-graph = {'start': {'a': 6, 'b': 2}, 'a': {'fin': 1}, 'b': {'a': 3, 'fin': 5}, 'fin': {}}
+graph = {'start': {'a': 6, 'b': 2}, 'a': {
+    'fin': 1}, 'b': {'a': 3, 'fin': 5}, 'fin': {}}
 costs = {'a': 6, 'b': 2, 'fin': float('inf')}
 parents = {'a': 'start', 'b': 'start', 'fin': None}
 
-
-
-
-
-
 processed = []
+
 
 def lowest_cost_node(costs):
     lower = float('inf')
@@ -50,21 +45,24 @@ def lowest_cost_node(costs):
     return lower_node
 
 
-node = lowest_cost_node(costs) # вершина с минимальным весом от начала(весом пути)
-while node is not None: # Пока все не обошли
+# вершина с минимальным весом от начала(весом пути)
+node = lowest_cost_node(costs)
+while node is not None:  # Пока все не обошли
     print(node)
-    cost = costs[node] # мин путь к этой точке
-    neighbors = graph[node] # его соседи
-    for n in neighbors.keys(): # перебор соседей
-        new_cost = cost + neighbors[n] # новый путь = мин путь + путь до соседа
-        if costs[n] > new_cost: # Если новый путь меньше, чем путь соседа с начала
-            costs[n] = new_cost # То Через точку соседа(А) быстрее, присваиваем ей путь от начала
-            parents[n] = node # и родителем соседа(А) ставим опорную точку Ноду. так как путеь будет через А
-    processed.append(node) # Отмечаем в пройденных вершинах
-    node = lowest_cost_node(costs) # Следующий меньший путь в необработанных
+    cost = costs[node]  # мин путь к этой точке
+    neighbors = graph[node]  # его соседи
+    for n in neighbors.keys():  # перебор соседей
+        # новый путь = мин путь + путь до соседа
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:  # Если новый путь меньше, чем путь соседа с начала
+            # То Через точку соседа(А) быстрее, присваиваем ей путь от начала
+            costs[n] = new_cost
+            # и родителем соседа(А) ставим опорную точку Ноду. так как путеь будет через А
+            parents[n] = node
+    processed.append(node)  # Отмечаем в пройденных вершинах
+    node = lowest_cost_node(costs)  # Следующий меньший путь в необработанных
 
 
 print(graph)
 print(costs)
 print(parents)
-
