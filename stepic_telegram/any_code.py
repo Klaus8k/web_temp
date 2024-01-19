9a60dd056901ed070330aa936ea2d12d2dc9f974
Oleg Sokolov <klaus888@yandex.ru>
2024-01-19 18:00:57 +0300

    teleg bot learning

---
+++ stepic_telegram/any_code.py
@@ -0,0 +1,6 @@
+
+
+
+anonymous_filter = lambda x: x.lower().count('я') >= 23
+
+print(anonymous_filter('яяяяяяяяяяяяяяяяяяяяяяяя, яяяяяяяяяяяяяяяя и яяяяяяяя тоже!'))
--- telegram/bot_telegram.py
+++ telegram/bot_telegram.py
@@ -1,58 +1,29 @@
-import asyncio
-import logging
-from random import randint
+import time
 
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.utils import exceptions
+import requests
 
-logging.basicConfig(level=logging.INFO)
+API_URL = "https://api.telegram.org/bot"
+BOT_TOKEN = "6241591643:AAH4GcwE_lgEbz0kQW3hLoMcr71iFTyiZnA"
+offset = -2
+timeout = 0
+updates: dict
+count = 0
 
-what_it_is = '6241591643'
-token_telegram = what_it_is + ":AAG3Fj6XQMejHi5-mLsc1rjKcKkyXDsYiYk"
-
-bot = Bot(token=token_telegram, parse_mode=types.ParseMode.HTML)
-
-dp = Dispatcher(bot=bot)
-
-# Хэндлер исключений
-bots_exeptions = [exceptions.BotBlocked,]
+def do_something() -> None:
+    print("Был апдейт")
 
 
-@dp.errors_handler(exception=bots_exeptions)
-async def errors_bot(update: types.Update, exeption: object):
-    pass
+while count < 10:
+    start_time = time.time()
+    updates = requests.get(
+        f"{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}&timeout={timeout}"
+    ).json()
 
+    if updates["result"]:
+        for result in updates["result"]:
+            offset = result["update_id"]
+            do_something()
 
-# Хэндлер ответа
-@dp.message_handler(commands=['start', 'help', '1-10'])
-async def cmd_start(message: types.Message):
-    print(message)
-    if message.get_command() == r'/start':
-        await message.reply('Get start!@')
-    elif message.get_command() == r'/help':
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        buttons = ['Menu', 'random', 'Order']
-        keyboard.add(*buttons)
-        await message.answer('Menu:', reply_markup=keyboard)
-
-
-@dp.message_handler(lambda message: message.text == 'random')
-async def random(message: types.Message):
-    keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(types.InlineKeyboardButton(
-        'rand 1-100', callback_data='start_rand'))
-    await message.answer('push the button', reply_markup=keyboard)
-
-
-@dp.callback_query_handler(text='start_rand')
-async def send_rand(call: types.CallbackQuery):
-    await call.message.answer(str(randint(1, 100)))
-
-
-@dp.message_handler(lambda message: message.text in ['order', 'Order'])
-async def order(message: types.Message):
-    await message.answer('Link to <a href="https://sokolol.ru/order_cost/">order</a>')
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    end_time = time.time()
+    print(f"Время между запросами к Telegram Bot API: {end_time - start_time}")
+    count += 5
