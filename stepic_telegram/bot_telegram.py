0228f792b1d497e745fc6d849136913f850ef524
Oleg <klaus888@yandex.ru>
2024-01-20 23:46:02 +0300

    refach

--- stepic_telegram/bot_telegram.py
+++ stepic_telegram/bot_telegram.py
@@ -3,7 +3,7 @@ import time
 import requests
 
 API_URL = "https://api.telegram.org/bot"
-BOT_TOKEN = "6241591643:AAH4GcwE_lgEbz0kQW3hLoMcr71iFTyiZnA"
+BOT_TOKEN = open('token', 'r').read()
 offset = -2
 timeout = 0
 updates: dict
