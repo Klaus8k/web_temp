d1a79e272c08a33407f995e9e7ae1027e091d32f
Oleg Sokolov <klaus888@yandex.ru>
2024-01-23 14:50:28 +0300

    logging

--- stepic_telegram/any_code.py
+++ stepic_telegram/any_code.py
@@ -1,6 +1,19 @@
+import logging
+import sys
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
+anonymous_filter = lambda x: x.lower().count("я") >= 23
 
-anonymous_filter = lambda x: x.lower().count('я') >= 23
+string = "яяяяяяяяяяяяяяяяяяяяяяяя, яяяяяяяяяяяяяяяя и яяяяяяяя тоже!"
+logger.debug("string" + string)
+logger.debug(logger.handlers)
+stderr_handler = logging.StreamHandler()
 
-print(anonymous_filter('яяяяяяяяяяяяяяяяяяяяяяяя, яяяяяяяяяяяяяяяя и яяяяяяяя тоже!'))
+stdout_handler = logging.StreamHandler(sys.stdout)
+
+logger.addHandler(stderr_handler)
+logger.addHandler(stdout_handler)
+print(logger.handlers)
+
