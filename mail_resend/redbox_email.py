3383a3c449dc154194387e1930b2b4672e90bf11
Klaus8k <klaus888@yandex.ru>
2024-02-03 15:17:13 +0300

    text and b64 add decode

--- mail_resend/redbox_email.py
+++ mail_resend/redbox_email.py
@@ -2,11 +2,14 @@ import pprint
 from redbox.query import UNSEEN, ALL
 from redbox import EmailBox
 from redmail import EmailSender
+from redbox.models.message import EmailMessage
+from typing import List, Type, Union
+
 import json
 import imaplib
 import ssl
 import base64
-import dill
+# import dill
 
 # https://red-mail.readthedocs.io/en/stable/
 # https://red-box.readthedocs.io/en/latest/
@@ -38,33 +41,41 @@ mails_list = []
 count = 0
 
 
-for msg in inbox.search(UNSEEN):
-    # if count == 1:
-        # break
-    sender_mail = msg.from_
-    mails_list.append(msg)
-    # count += 1
+def write_message(messages: List[EmailMessage]) -> None:
 
-# for msg in mails_list:
-#     x = dill.dumps(msg, protocol=4)
-#     pprint.pprint(x)
-#     # with dill.detect.trace():
-#     #     dill.dumps(msg)
+    for i in messages:
+        print(i.headers['Content-Type'])
+        if 'multipart/mixed' in i.headers['Content-Type']: # тип контакта в хедере
+            if i.html_body.startswith('<'): # если, начинается html c '<'
+                print('header - --------------------', i.html_body)
+                mails_list.append(i.html_body) 
+                continue
+            print(i.html_body)
+            body_mail_b64 = base64.b64decode(i.html_body).decode()
+            print(body_mail_b64)
+            mails_list.append(body_mail_b64)
+        elif 'text/plain' in i.headers['Content-Type']:
+            print('header - --------------------', i.html_body)
+            mails_list.append(i.html_body)
 
-# msg = mails_list[0]
-# pprint.pprint(msg.__dir__())
-# open('from.txt', 'w').write(pprint.pformat(msg.headers))
 
-# import traceback
-# for _ in msg.headers.keys():
-#     msg.headers[_] = ''.join(msg.headers[_].split('\r\n'))
 
+msg_list = []
+
+for msg in inbox.search(ALL):
+    msg_list.append(msg)
+
+
+write_message(msg_list)
+pprint.pprint(mails_list)
 
 
 for i in mails_list:
+    html_mail = i
+    # subject = base64.b64decode(i.headers['Subject']).decode()
     sender.send(
         # headers=i_headers,
-        html=i.html_body,
+        html=html_mail,
         # sender=msg.from_,
         subject="resend",
         receivers=[
